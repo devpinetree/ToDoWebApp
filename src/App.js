3f7b27e7db934403a2cdfa223757f86b0fe4285d
bosollee <bosollee@gmail.com>
2020-04-25 19:33:36 +0900

    Add Insert Todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'react1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'react2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'react3',
+      checked: false,
+    },
+    {
+      id: 4,
+      text: 'react4',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
